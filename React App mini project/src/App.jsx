de05005b3c360d2bed6ce36b25b3ab2dce04a62c
“Carina <ksfraan@hotmail.com>
2023-12-12 18:13:01 +0000

    add a list

--- React App mini project/src/App.jsx
+++ React App mini project/src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
-import "./App.css";
-import Navbar from "./assets/components/Navbar";
-import Footer from "./assets/components/Footer";
-import Sidebar from "./assets/components/Sidebar";
+import { useState } from 'react';
+import './App.css';
+import Navbar from './assets/components/Navbar';
+import Footer from './assets/components/Footer';
+import Sidebar from './assets/components/Sidebar';
+import ProductsList from './assets/components/ProductsList';
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <Footer />
-      </div>
-      <div>
-        <Sidebar />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <Footer />
+            </div>
+            <div>
+                <Sidebar />
+            </div>
+            <div>
+                {' '}
+                <ProductsList />
+            </div>
+        </div>
+    );
 }
 
 export default App;
