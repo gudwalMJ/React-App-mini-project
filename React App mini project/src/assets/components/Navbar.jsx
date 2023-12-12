88baf5cf1fd0000c9871826888de82b025e043b4
Josip Milan <josip.milan@aol.com>
2023-12-12 17:08:28 +0100

    navBar

--- React App mini project/src/assets/components/Navbar.jsx
+++ React App mini project/src/assets/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import ironhacklogo from '../images/ironhacklogo.png';
+import "/src/style/navBar.css";
+import ironhacklogo from "../images/ironhacklogo.png";
 
 function Navbar() {
-    return (
-        <div className='navBar'>
-            <h1>Ironhack Market</h1>
-            <img src={ironhacklogo} alt='Ironhack Logo' />
-        </div>
-    );
+  return (
+    <div className="navBar">
+      <img src={ironhacklogo} alt="Ironhack Logo" />
+      <h1>Ironhack Market</h1>
+    </div>
+  );
 }
 
 export default Navbar;
