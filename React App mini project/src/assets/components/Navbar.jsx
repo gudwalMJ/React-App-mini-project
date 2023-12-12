e528ba22bc8a982cea35068acbda133a4968d7ab
“Carina <ksfraan@hotmail.com>
2023-12-12 15:48:49 +0000

    second commit

--- React App mini project/src/assets/components/Navbar.jsx
+++ React App mini project/src/assets/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import ironhacklogo from '../images/ironhacklogo.png';
 
 function Navbar() {
-  return (
-    <div className="navBar">
-      <h1>Ironhack Market</h1>
-      <img src="./assets/images/ironhacklogo" alt="Ironhack Logo" />
-    </div>
-  );
+    return (
+        <div className='navBar'>
+            <h1>Ironhack Market</h1>
+            <img src={ironhacklogo} alt='Ironhack Logo' />
+        </div>
+    );
 }
 
 export default Navbar;
