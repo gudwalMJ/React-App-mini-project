d84ee1c0f2f27182bd2c939186738a52cba01fb8
Josip Milan <josip.milan@aol.com>
2023-12-12 19:24:26 +0100

    css for products

--- React App mini project/src/assets/components/ProductsList.jsx
+++ React App mini project/src/assets/components/ProductsList.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import productsData from '/products.json';
+import React, { useState } from "react";
+import productsData from "/products.json";
+import "/src/style/productsList.css";
 
 function ProductList() {
-    const [products, setProducts] = useState(productsData);
-    console.log(products);
-    return (
-        <div>
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id} className='product-item'>
-                        <img src={product.thumbnail} alt={product.title} />
-                        <div className='textPart'>
-                            <h3>{product.title}</h3>
-                            <p>{product.description}</p>
-                            <p>
-                                <span className='bold-text'>Price:</span> $
-                                {product.price}
-                            </p>
-                            {/* Add any additional information you want to display */}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const [products, setProducts] = useState(productsData);
+  console.log(products);
+  return (
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id} className="product-item">
+            <img src={product.thumbnail} alt={product.title} />
+            <div className="textPart">
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>
+                <span className="bold-text">Price:</span> ${product.price}
+              </p>
+              {/* Add any additional information you want to display */}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductList;
