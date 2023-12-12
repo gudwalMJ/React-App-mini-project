f4501c69520210bccb103732df054c4798b60d19
Josip Milan <josip.milan@aol.com>
2023-12-12 17:53:10 +0100

    sideBar

--- React App mini project/src/App.jsx
+++ React App mini project/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './assets/components/Navbar';
-import Footer from './assets/components/Footer';
-import Sidebar from './assets/components/Sidebar';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./assets/components/Navbar";
+import Footer from "./assets/components/Footer";
+import Sidebar from "./assets/components/Sidebar";
 
 function App() {
-    return (
-        <div>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <Footer />
-            </div>
-            <div>
-                <Sidebar />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Footer />
+      </div>
+      <div>
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- React App mini project/src/assets/components/Sidebar.jsx
+++ React App mini project/src/assets/components/Sidebar.jsx
@@ -1,18 +1,20 @@
+import "/src/style/sideBar.css";
+
 function Sidebar() {
-    return (
-        <div className='Sidebar'>
-            <nav>
-                <ul>
-                    <li>
-                        <a href='#'>Home Page</a>
-                    </li>
-                    <li>
-                        <a href='#'>About</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="sideBar">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home Page</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Sidebar;
