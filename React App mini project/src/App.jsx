9e69a5865648a501ea6bca0ee8533c14859cb92a
“Carina <ksfraan@hotmail.com>
2023-12-13 17:55:09 +0000

    add a condition

--- React App mini project/src/App.jsx
+++ React App mini project/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./assets/components/Navbar";
@@ -25,6 +26,34 @@ function App() {
       </div>
     </div>
   );
+=======
+import React from 'react';
+import './App.css';
+import Navbar from './assets/components/Navbar';
+import Footer from './assets/components/Footer';
+import Sidebar from './assets/components/Sidebar';
+import ProductsList from './assets/components/ProductsList';
+
+function App() {
+    return (
+        <>
+            <div>
+                <div>
+                    <Navbar />
+                </div>
+                <div>
+                    <Footer />
+                </div>
+                <div>
+                    <Sidebar />
+                </div>
+            </div>
+            <div>
+                <ProductsList />
+            </div>
+        </>
+    );
+>>>>>>> 59f3ad6 (add a list of products)
 }
 
 export default App;
--- React App mini project/src/assets/components/ProductsList.jsx
+++ React App mini project/src/assets/components/ProductsList.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
-import productsData from "/products.json";
-import "/src/style/productsList.css";
+import React, { useState } from 'react';
+import productsData from './products.json';
+import './src/style/productsList.css';
 
 function ProductList() {
-  const [products, setProducts] = useState(productsData);
-  console.log(products);
-  return (
-    <div>
-      <ul className="product-list">
-        {products.map((product) => (
-          <li key={product.id} className="product-item">
-            <img src={product.thumbnail} alt={product.title} />
-            <div className="textPart">
-              <h3>{product.title}</h3>
-              <p>{product.description}</p>
-              <p>
-                <span className="bold-text">Price:</span> ${product.price}
-              </p>
-              {/* Add any additional information you want to display */}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const [products, setProducts] = useState(productsData);
+
+    const defaultStockAmount = 60;
+
+    return (
+        <div className='productsList-wrapper'>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id} className='product-item'>
+                        {product.stock < defaultStockAmount && (
+                            <span key='low-stock' className='low stock'>
+                                LOW STOCK
+                            </span>
+                        )}
+                        <img src={product.thumbnail} alt={product.title} />
+                        <div className='textPart'>
+                            <h3>{product.title}</h3>
+                            <p>{product.description}</p>
+                            <p>Stock: {product.stock}</p>
+                            <p>
+                                <span className='bold-text'>Price:</span> $
+                                {product.price}
+                            </p>
+                            {/* Add any additional information you want */}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ProductList;
