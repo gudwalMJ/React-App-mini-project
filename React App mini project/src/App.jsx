31949682502ae40955bcc36cca31e65240393390
“Carina <ksfraan@hotmail.com>
2023-12-13 17:56:43 +0000

    add a condition

--- React App mini project/src/App.jsx
+++ React App mini project/src/App.jsx
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import "./App.css";
-import Navbar from "./assets/components/Navbar";
-import Footer from "./assets/components/Footer";
-import Sidebar from "./assets/components/Sidebar";
-import ProductsList from "./assets/components/ProductsList";
-
-function App() {
-  return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <Footer />
-      </div>
-      <div>
-        <div>
-          <Sidebar />
-        </div>
-        <div>
-          {" "}
-          <ProductsList />
-        </div>
-      </div>
-    </div>
-  );
-=======
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './assets/components/Navbar';
 import Footer from './assets/components/Footer';
@@ -36,24 +7,24 @@ import ProductsList from './assets/components/ProductsList';
 
 function App() {
     return (
-        <>
+        <div>
             <div>
-                <div>
-                    <Navbar />
-                </div>
-                <div>
-                    <Footer />
-                </div>
+                <Navbar />
+            </div>
+            <div>
+                <Footer />
+            </div>
+            <div>
                 <div>
                     <Sidebar />
                 </div>
+                <div>
+                    {' '}
+                    <ProductsList />
+                </div>
             </div>
-            <div>
-                <ProductsList />
-            </div>
-        </>
+        </div>
     );
->>>>>>> 59f3ad6 (add a list of products)
 }
 
 export default App;
