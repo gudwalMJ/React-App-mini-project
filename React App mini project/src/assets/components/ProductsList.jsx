f9e59493668c2328473b83ac4c88855df797bb9f
“Carina <ksfraan@hotmail.com>
2023-12-18 17:31:16 +0000

    updates

--- React App mini project/src/assets/components/ProductsList.jsx
+++ React App mini project/src/assets/components/ProductsList.jsx
@@ -1,19 +1,19 @@
-import List from "./List";
-import "/src/style/productsList.css";
+import List from './List';
+import '/src/style/productsList.css';
 
 function ProductsList({ products, onDelete, setProducts }) {
-  console.log("ProductsList rendering with products:", products);
-  const handleDelete = (productId) => {
-    const updatedProducts = products.filter(
-      (product) => product.id !== productId
+    console.log('ProductsList rendering with products:', products);
+    const handleDelete = (productId) => {
+        const updatedProducts = products.filter(
+            (product) => product.id !== productId
+        );
+        setProducts(updatedProducts);
+    };
+    return (
+        <div className='product-list-wrapper'>
+            <h2>Product List</h2>
+            <List products={products} handleDelete={handleDelete} />
+        </div>
     );
-    setProducts(updatedProducts);
-  };
-  return (
-    <div className="product-list-wrapper">
-      <h2>Product List</h2>
-      <List products={products} handleDelete={handleDelete} />
-    </div>
-  );
 }
 export default ProductsList;
