25301ee436b840a3e672a972a9ccb9a8f77a1d55
Josip Milan <josip.milan@aol.com>
2023-12-18 18:02:15 +0100

    updates

--- React App mini project/src/assets/pages/AboutPage.jsx
+++ React App mini project/src/assets/pages/AboutPage.jsx
@@ -1,60 +1,68 @@
-import { Link } from 'react-router-dom';
-import Carina from '../images/CarinaFrança.jpeg';
-import Josip from '../images/JosipMilan.jpg';
+import { Link } from "react-router-dom";
+import Carina from "../images/CarinaFrança.jpeg";
+import Josip from "../images/JosipMilan.jpg";
 
 const AboutPage = () => {
-    return (
-        <div className='about-page'>
-            {' '}
-            <h1 className='welcome-text'> Welcome to IronHack Market </h1>
-            <h3>Information about the team members:</h3>
-            <ul className='team-members'>
-                <li>
-                    <img
-                        src={Carina}
-                        alt='photo-carina'
-                        className='photo-carina'
-                    />
-                    <br />
-                    <p>
-                        <strong>Carina França</strong> I&apos;m Brazilian living
-                        in Portugal, I&apos;m 28 years old, with a background in
-                        pharmacy, I&apos;m changing careers to IT and really
-                        enjoying the process. You can check my GitHub profile{' '}
-                        <Link to='https://github.com/settings/profile'>
-                            here{' '}
-                        </Link>
-                        and my Linkedin profile
-                        <Link to='#'> here</Link>
-                    </p>
-                </li>
-                <li>
-                    {' '}
-                    <img
-                        src={Josip}
-                        alt='Photo Josip'
-                        className='photo-Josip'
-                    />
-                    <p>
-                        {' '}
-                        <strong>Josip Milan</strong> A 32-year-old Croatian
-                        currently residing in the Netherlands, I have
-                        transitioned from a previous career as a chef to embark
-                        on a new journey in the field of IT. Embracing the
-                        challenges of this career switch, I am thoroughly
-                        enjoying the learning process and the opportunities it
-                        brings. You can check my GitHub profile
-                        <Link to='https://github.com/gudwalMJ'>here </Link>and
-                        my Linkedin profile
-                        <Link to='www.linkedin.com/in/josip-milan-4738a9183'>
-                            {' '}
-                            here
-                        </Link>{' '}
-                    </p>{' '}
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="about-page">
+      <h1 className="welcome-text">Welcome to IronHack Marketplace</h1>
+      <h3 className="info">Information about the team members:</h3>
+      <ul className="team-members">
+        <li className="team-member">
+          <img src={Carina} alt="Carina França" className="team-member-photo" />
+          <div className="team-member-info">
+            <strong>Carina França</strong> I'm Brazilian living in Portugal, I'm
+            28 years old, with a background in pharmacy, I'm changing careers to
+            IT and really enjoying the process.
+            <div>
+              <br />
+              You can check my GitHub profile{" "}
+              <a
+                href="https://github.com/settings/profile"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>{" "}
+              and my LinkedIn profile{" "}
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                here
+              </a>
+            </div>
+          </div>
+        </li>
+        <li className="team-member">
+          <img src={Josip} alt="Josip Milan" className="team-member-photo" />
+          <div className="team-member-info">
+            <strong>Josip Milan</strong> A 32-year-old Croatian currently
+            residing in the Netherlands, I have transitioned from a previous
+            career as a chef to embark on a new journey in the field of IT.
+            Embracing the challenges of this career switch, I am thoroughly
+            enjoying the learning process and the opportunities it brings.
+            <div>
+              <br />
+              You can check my GitHub profile{" "}
+              <a
+                href="https://github.com/gudwalMJ"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>{" "}
+              and my LinkedIn profile{" "}
+              <a
+                href="www.linkedin.com/in/josip-milan-4738a9183"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default AboutPage;
